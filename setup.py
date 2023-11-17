3b0db2a99fa2aee3ac369ceec968cfad21741d14
zetro <83398581+zetrobt@users.noreply.github.com>
2023-11-17 16:22:11 +0500

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="Eljur",
+    version="1.0",
+    description="Eljur.ru diary API Wrapper",
+    url="https://github.com/zetrobt/eljur",
+    download_url="https://github.com/zetrobt/eljur/releases/latest",
+    author="zetro.",
+    author_email="me@zetro.ga",
+    license="MIT",
+    python_requires="~=3.7",
+    packages=find_packages(),
+    zip_safe=False,
+    install_requires=[]
+)
